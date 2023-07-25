fdf8cb6000e15f37d5b0da11ea314e91cb7281ae
jxintao <763421217@qq.com>
2023-07-25 10:27:56 +0800

    chatgpt提示语优化8

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -50,24 +50,105 @@ class EventController extends Controller
             if ($data['from_user_id'] == $data['to_user_id']) {
                 return 'ok';
             }
+            // 判断是否是回复我的，并组合上下文
+            $context = [];
+            if (!empty($eventContent['reply_to_comment_id'])) {
+                $existComment1 = Comment::where('comment_id', $eventContent['reply_to_comment_id'])->first();
+                if ($existComment1 && $existComment1->comment_user_id != $accountConfig->user_id) {
+                    return 'ok';
+                }
+                if ($existComment1 && $existComment1->comment_user_id == $accountConfig->user_id) {
+                    $context[] = [
+                        "role"    => "assistant",
+                        "content" => $existComment1->content
+                    ];
+                }
+                if (!empty($existComment1->reply_to_comment_id)) {
+                    $existComment2 = Comment::where('comment_id', $existComment1->reply_to_comment_id)->first();
+                    if ($existComment2 && $existComment2->comment_user_id != $accountConfig->user_id) {
+                        $context[] = [
+                            "role"    => "user",
+                            "content" => $existComment2->content
+                        ];
+                    }
+                    if ($existComment2 && $existComment2->comment_user_id == $accountConfig->user_id) {
+                        $context[] = [
+                            "role"    => "assistant",
+                            "content" => $existComment2->content
+                        ];
+                    }
+                }
+                if (!empty($existComment2->reply_to_comment_id)) {
+                    $existComment3 = Comment::where('comment_id', $existComment2->reply_to_comment_id)->first();
+                    if ($existComment3 && $existComment3->comment_user_id != $accountConfig->user_id) {
+                        $context[] = [
+                            "role"    => "user",
+                            "content" => $existComment3->content
+                        ];
+                    }
+                    if ($existComment3 && $existComment3->comment_user_id == $accountConfig->user_id) {
+                        $context[] = [
+                            "role"    => "assistant",
+                            "content" => $existComment3->content
+                        ];
+                    }
+                }
+                if (!empty($existComment3->reply_to_comment_id)) {
+                    $existComment4 = Comment::where('comment_id', $existComment3->reply_to_comment_id)->first();
+                    if ($existComment4 && $existComment4->comment_user_id != $accountConfig->user_id) {
+                        $context[] = [
+                            "role"    => "user",
+                            "content" => $existComment4->content
+                        ];
+                    }
+                    if ($existComment4 && $existComment4->comment_user_id == $accountConfig->user_id) {
+                        $context[] = [
+                            "role"    => "assistant",
+                            "content" => $existComment4->content
+                        ];
+                    }
+                }
+                if (!empty($existComment4->reply_to_comment_id)) {
+                    $existComment5 = Comment::where('comment_id', $existComment4->reply_to_comment_id)->first();
+                    if ($existComment5 && $existComment5->comment_user_id != $accountConfig->user_id) {
+                        $context[] = [
+                            "role"    => "user",
+                            "content" => $existComment5->content
+                        ];
+                    }
+                    if ($existComment5 && $existComment5->comment_user_id == $accountConfig->user_id) {
+                        $context[] = [
+                            "role"    => "assistant",
+                            "content" => $existComment5->content
+                        ];
+                    }
+                }
+            }
             // 调用chatGpt接口
             try {
                 $url = 'https://api.openai-proxy.com/v1/chat/completions';
-                $systemRoleContent = !empty($accountConfig->system_prompt) ? $accountConfig->system_prompt : '我是 ' . $accountConfig->nickname. ' 的智能助手，我会帮Ta回复评论。回复内容总长度绝对不能超过100个字符。';
-                $userRoleContent = $eventContent['content'] . '。 回复内容总长度不能超过100个字符。';
+                $systemRoleContent = !empty($accountConfig->system_prompt) ? $accountConfig->system_prompt : '我是 ' . $accountConfig->nickname . ' 的智能助手，我会帮Ta回复评论。回复内容总长度绝对不能超过100个字符。';
+                $userRoleContent = !empty($eventContent['content']) ? $eventContent['content'] : '讲个笑话';
+                $userRoleContent = $userRoleContent . '。 回复内容总长度不能超过100个字符。';
                 $data = [
                     "model"    => "gpt-3.5-turbo",
                     "messages" => [
                         [
                             "role"    => "system",
                             "content" => $systemRoleContent
-                        ],
-                        [
-                            "role"    => "user",
-                            "content" => $userRoleContent
                         ]
                     ]
                 ];
+                if (!empty($context)) {
+                    $context = array_reverse($context);
+                    $data['messages'] = array_merge($data['messages'], $context);
+                }
+                $data['messages'][] = [
+                    "role"    => "user",
+                    "content" => $userRoleContent
+                ];
+                // 写入日志
+                Log::error('chatGptRequest', [$data]);
                 $chatGptResponse = $requestService->httpPostOpenAi($url, $data, $accountConfig->openai_key);
                 $chatGptResponse = json_decode($chatGptResponse, true);
                 // 写入日志
